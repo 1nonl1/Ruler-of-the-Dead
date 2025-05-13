835f0d529a9b9f306944a8c0179ae1eee12169c8
1nonly <tobiasbraymen@gmail.com>
2025-05-13 00:17:14 +0000

    git commit

--- pythonRPG/main.py
+++ pythonRPG/main.py
@@ -21,8 +21,13 @@ def save():
     if not os.path.exists('pythonRPG/saveFile.pkl'):
         with open('pythonRPG/saveFile.pkl', 'wb') as f:
             pickle.dump([env, player, inv], f)
-    os.chmod('pythonRPG/saveFile.pkl', 0o666)
-
+        os.chmod('pythonRPG/saveFile.pkl', 0o666)
+    elif os.path.exists('pythonRPG/saveFile.pkl'):
+        with open('pythonRPG/saveFile.pkl', 'wb') as f:
+            pickle.dump([env, player, inv], f)
+        os.chmod('pythonRPG/saveFile.pkl', 0o666)
+    else:
+        pass
 if __name__ == "__main__":
     initialize()
     try: 
